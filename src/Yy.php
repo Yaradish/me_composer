685e5f62184451676c86a965aeb18bbfbf71f034
yangchenguang <yangchenguang@hrgame.com.cn>
2021-12-13 16:35:44 +0800

    composer

---
+++ src/Yy.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: YCG
+ * Date: 2021/12/13
+ * Time: 16:24
+ **/
+
+namespace Yy\Yy;
+
+class Yy
+{
+    public function __construct()
+    {
+    }
+
+    public function hello()
+    {
+        return 'hello world!!';
+    }
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: YCG
+ * Date: 2021/12/13
+ * Time: 16:27
+ **/
+require_once __DIR__ . '/vendor/autoload.php';
+use Yy\Yy\Yy;
+
+$obj = new Yy();
+
+$msg = $obj->hello();
+var_dump($msg);
